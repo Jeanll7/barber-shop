3fa81d492d79d4996f4147b6e58d0b04e2ec2e2b
Jeanll7 <jeancarloleall@gmail.com>
2024-08-07 10:52:08 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,37 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className="bg-blue-500 px-4 py-4 text-base text-white">
-      <h1>Barbearia</h1>
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Jean</h2>
+        <p>Segunda-feira, 05 de agosto</p>
+
+        <div className="mt-6 flex justify-between gap-2">
+          <Input
+            className="focus:outline-none"
+            placeholder="Faça sua busca..."
+          />
+          <Button size="icon">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Agende nos melhores barbeiros"
+          />
+        </div>
+      </div>
     </div>
   )
 }
