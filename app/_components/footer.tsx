2b9eaa5dfb449f020850d1a7574809fb2323b78a
Jeanll7 <jeancarloleall@gmail.com>
2024-08-10 20:46:22 -0300

    feat: center Footer content using justify-center

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -3,11 +3,9 @@ import { Card, CardContent } from "./ui/card"
 const Footer = () => {
   return (
     <footer>
-      <Card className="mt-8 rounded-none">
+      <Card className="mt-8 flex justify-center rounded-none">
         <CardContent className="px-5 py-6">
-          <p className="text-sm text-gray-400">
-            © 2024 Copyrigght Barber Shop
-          </p>
+          <p className="text-sm text-gray-400">©2024 Copyright Barber Shop</p>
         </CardContent>
       </Card>
     </footer>
